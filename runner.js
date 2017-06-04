9aa9cd2795e83209eb2b61104f4997bf0945dc46
Rober Gerszenswit <rober.gerszen@gmail.com>
2017-06-04 19:30:33 -0300

    añadidos comentarios a runner.js

--- runner.js
+++ runner.js
@@ -1,19 +1,25 @@
 var Runner = function(name, historyPositions) {
+  //el nombre del corredor
     this.name = name;
+  //el arreglo de posiciones en las que estuvo
     this.historyPositions = historyPositions;
 
-    var actualIx = 0;
+//inicialización del indice para recorrer el arreglo de posiciones
+//historicas
+    var indiceActual = 0;
 
+//function que recorre las posiciones una a una
+//ejecutando la function callback pasada como parametro
+//(esta function seguramente dibuja en el mapa.)
     this.run = function(callback) {
         var self = this;
         setTimeout(function() {
-            callback(historyPositions[actualIx]);
+            callback(historyPositions[indiceActual]);
 
-            actualIx += 1;
-            if(actualIx < historyPositions.length) {
+            indiceActual += 1;
+            if(indiceActual < historyPositions.length) {
                 self.run(callback);
             }
-        }, 1000);
+        }, 1000);//1000 ms -> 1 s, la function se ejecuta cada 1 segundo.
     }
 };
-
